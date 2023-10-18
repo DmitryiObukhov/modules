7e8cb8eafde2cc634e969bab711075504fdd8bc4
dmitryi <vdv404901@gmail.com>
2023-10-18 19:16:07 +0300

    modules

---
+++ example.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Create a 2-D array, set every second element in
+# some rows and find max per row:
+
+x = np.arange(15, dtype=np.int64).reshape(3, 5)
+x[1:, ::2] = -99
+print("Modified array x:")
+print(x)
+
+max_values = x.max(axis=1)
+print("Max values per row:")
+print(max_values)
+
+# Generate normally distributed random numbers:
+rng = np.random.default_rng()
+samples = rng.normal(size=2500)
+print("Generated random samples:")
+print(samples)
---
+++ main.py
@@ -0,0 +1,4 @@
+from hello import hello as hi
+
+user_name = input("Enter your name: ")
+hi(user_name)
