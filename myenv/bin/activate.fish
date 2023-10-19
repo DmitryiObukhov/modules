8c8ba30332bb0d137c227b3f49bd697daca8c5a0
dmitryi <vdv404901@gmail.com>
2023-10-19 14:13:56 +0300

    modules

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dima/Рабочий стол/python prac#14/myenv"
+set -gx VIRTUAL_ENV "/home/dima/Рабочий стол/modules/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
